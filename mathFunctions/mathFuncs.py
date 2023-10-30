5ad7b0bb240c71c391fd659685c9978710127e2a
Eduardo Alejandro González Quintero <113394223+EduardoGlez01@users.noreply.github.com>
2023-10-30 15:01:58 -0400

    commit

---
+++ mathFunctions/mathFuncs.py
@@ -0,0 +1,14 @@
+##function to add two numbers
+def addNumbers (number1, number2):
+    '''
+    provide 2 numbers, returns an added number
+    '''
+    return number1 + number2
+
+
+##function to substract two numbers
+def subNumbers (number1, number2):
+    '''
+    provide 2 numbers, returns an substracted number
+    '''
+    return number1 - number2
